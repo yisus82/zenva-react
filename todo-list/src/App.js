80e7195a173efef0f0f61734ba31e8d463454388
Jesús Ángel Pérez-Roca Fernández <yisus82@users.noreply.github.com>
2019-12-12 11:50:18 +0100

    Added state

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Table, Checkbox, Button, Icon } from 'semantic-ui-react';
 import './App.css';
 
-const todos = [
-  'Learn React',
-  'Learn Redux',
-  'Learn React Native',
-  'Create a brand new web app!',
+const initialTodos = [
+  {
+    title: 'Learn React',
+    completed: false,
+  },
+  {
+    title: 'Learn Redux',
+    completed: false,
+  },
+  {
+    title: 'Learn React Native',
+    completed: false,
+  },
+  {
+    title: 'Create a brand new web app!',
+    completed: false,
+  },
 ];
 
-const TodoItem = ({ text }) => (
+const TodoItem = ({ title, completed, checkTodo, removeTodo }) => (
   <Table.Row>
     <Table.Cell>
-      <Checkbox />
+      <Checkbox checked={completed} onClick={() => checkTodo(title)} />
     </Table.Cell>
-    <Table.Cell>{text}</Table.Cell>
+    <Table.Cell>{title}</Table.Cell>
     <Table.Cell>
-      <Button animated color="red">
+      <Button
+        animated
+        color="red"
+        size="tiny"
+        onClick={() => removeTodo(title)}
+      >
         <Button.Content hidden>Delete</Button.Content>
         <Button.Content visible>
           <Icon name="trash" />
@@ -28,37 +45,143 @@ const TodoItem = ({ text }) => (
 );
 
 TodoItem.propTypes = {
-  text: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired,
+  checkTodo: PropTypes.func.isRequired,
+  removeTodo: PropTypes.func.isRequired,
 };
 
-const App = () => (
-  <div className="app">
-    <div className="todo-container">
-      <label htmlFor="new-todo" style={{ display: 'none' }}>
-        New Todo
+const TodosTable = ({
+  todos,
+  setTodos,
+  areAllChecked,
+  checkTodo,
+  removeTodo,
+}) => (
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>
+            <Checkbox
+              checked={areAllChecked()}
+              onClick={() =>
+                setTodos(() => {
+                  const check = !areAllChecked();
+                  return todos.map(todo => ({ ...todo, completed: check }));
+                })
+              }
+            />
+          </Table.HeaderCell>
+          <Table.HeaderCell colSpan="2">Complete all</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {todos.map(({ title, completed }) => (
+          <TodoItem
+            key={title}
+            title={title}
+            completed={completed}
+            checkTodo={checkTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+      </Table.Body>
+    </Table>
+  );
+
+TodosTable.propTypes = {
+  todos: PropTypes.array.isRequired,
+  setTodos: PropTypes.func.isRequired,
+  areAllChecked: PropTypes.func.isRequired,
+  checkTodo: PropTypes.func.isRequired,
+  removeTodo: PropTypes.func.isRequired,
+};
+
+const Header = ({ createTodo }) => {
+  const [title, setTitle] = useState('');
+  const [error, setError] = useState('');
+
+  return (
+    <>
+      <div style={{ display: 'flex' }}>
         <input
           id="new-todo"
           className="new-todo"
           placeholder="What needs to be done?"
+          aria-label="New Todo"
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+          style={{ marginRight: "1rem" }}
         />
-      </label>
-      <Table>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>
-              <Checkbox />
-            </Table.HeaderCell>
-            <Table.HeaderCell colSpan="2">Select all</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {todos.map((todo, i) => (
-            <TodoItem key={i} text={todo} />
-          ))}
-        </Table.Body>
-      </Table>
+        <Button
+          animated
+          color="blue"
+          size="tiny"
+          onClick={() => {
+            if (createTodo(title)) {
+              setTitle('');
+              setError('');
+            } else {
+              setError('Duplicated ToDo title');
+            }
+          }}
+        >
+          <Button.Content hidden>Create</Button.Content>
+          <Button.Content visible>
+            <Icon name="add" />
+          </Button.Content>
+        </Button>
+      </div>
+      <p style={{ color: 'red', margin: "1rem 0" }}>{error}</p>
+    </>
+  );
+};
+
+Header.propTypes = {
+  createTodo: PropTypes.func.isRequired,
+};
+
+const App = () => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const areAllChecked = () => !todos.find(todo => !todo.completed);
+
+  const checkTodo = title =>
+    setTodos(
+      todos.map(todo =>
+        todo.title === title ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+
+  const removeTodo = title =>
+    setTodos(todos.filter(todo => todo.title !== title));
+
+  const findTodo = title => todos.find(todo => !todo.title.localeCompare(title, undefined, { sensitivity: 'accent' }));
+
+  const createTodo = title => {
+    if (!title || findTodo(title)) {
+      return false;
+    }
+
+    const todo = { title, completed: false };
+    setTodos([...todos, todo]);
+    return true;
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-container">
+        <Header createTodo={createTodo} />
+        <TodosTable
+          todos={todos}
+          setTodos={setTodos}
+          areAllChecked={areAllChecked}
+          checkTodo={checkTodo}
+          removeTodo={removeTodo}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
